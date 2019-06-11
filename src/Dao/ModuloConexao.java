3a256475c0a1aeb87fac39c7d2f8ef3481b0d3e5
Andre Franklin <Andre Franklin@Notebook>
2019-06-10 21:05:49 -0300

    Commit inicial da versão 1.0 do sistema. Conexão com o banco configuradapara o servidor.

--- src/Dao/ModuloConexao.java
+++ src/Dao/ModuloConexao.java
@@ -14,13 +14,13 @@ public class ModuloConexao {
         String Driver = "com.mysql.jdbc.Driver";
         //Armazenando informaçoes referente ao banco.   
         //----------------------------------------------------------
-        String url = "jdbc:mysql://localhost:3306/db_multisystem";
-        String user = "root";
-        String password = ""; 
+        //String url = "jdbc:mysql://localhost:3306/db_multisystem";
+        //String user = "root";
+        //String password = ""; 
         //----------------------------------------------------------
-        //String url = "jdbc:mysql://172.30.70.18/db_multisystem";
-        //String user = "tecnico";
-        //String password = "123mudar!";
+        String url = "jdbc:mysql://172.30.70.18/db_multisystem";
+        String user = "tecnico";
+        String password = "123mudar!";
         //---------------------------------------------------------------------
         //String url = "jdbc:mysql://mysql995.umbler.com:41890/db_multisystem"; 
                 
